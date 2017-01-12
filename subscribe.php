e6a7e21b8ae963d606b1e56a48da45b1ef99ac3f
cccampbell <cam1997campbell@googlemail.com>
2017-01-12 12:34:08 +0000

    Refactored topics

--- subscribe.php
+++ subscribe.php
@@ -4,6 +4,9 @@ require("models/phpMQTT.php");
 
 $host = "146.87.2.99";
 $port = "1883";
+date_default_timezone_set('Europe/London');
+$date_timezone = date_default_timezone_get();
+
 
 
 $mqtt = new phpMQTT($host, $port, "ClientID".rand());
@@ -11,13 +14,21 @@ $mqtt = new phpMQTT($host, $port, "ClientID".rand());
 if(!$mqtt->connect()){
     exit(1);
 }
-$topics['/temp'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/humidity'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/air_pressure'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/rain'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/wind_s'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/wind_d'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/solar'] = array("qos"=>0, "function"=>"procmsg");
+
+$weather_factors = array("temp", "humidity", "air_pressure", "rain", "wind_s", "wind_d", "solar");
+
+foreach ($weather_factors as $factor) {
+    $topics['/'.$factor] = array("qos"=>0, "function"=>"procmsg");
+}
+
+
+//$topics['/temp'] = array("qos"=>0, "function"=>"procmsg");
+//$topics['/humidity'] = array("qos"=>0, "function"=>"procmsg");
+//$topics['/air_pressure'] = array("qos"=>0, "function"=>"procmsg");
+//$topics['/rain'] = array("qos"=>0, "function"=>"procmsg");
+//$topics['/wind_s'] = array("qos"=>0, "function"=>"procmsg");
+//$topics['/wind_d'] = array("qos"=>0, "function"=>"procmsg");
+//$topics['/solar'] = array("qos"=>0, "function"=>"procmsg");
 
 $mqtt->subscribe($topics,0);
 while($mqtt->proc()){
@@ -25,6 +36,6 @@ while($mqtt->proc()){
 }
 $mqtt->close();
 function procmsg($topic,$msg){
-    echo "Msg Recieved: ".date("r")."\nTopic:{$topic}\n$msg\n";
+    echo "Msg Recieved: ".date("h:i:sa")."\nTopic:{$topic}\n$msg\n";
 }
 
