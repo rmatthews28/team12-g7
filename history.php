787a1c81e1d944770e6b03e0fa510d5c0fe2e305
ArifK1994 <m702@hormail.co.uk>
2017-01-17 11:25:48 +0000

    Range slider implemented and code cleanup for subscribe

--- history.php
+++ history.php
@@ -1,28 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: CameronCampbell
- * Date: 13/01/2017
- * Time: 16:21
- */
-require("history.phtml");
+<?php require("history.phtml");
+
+if (isset($_POST["passengers"])) {
+//    echo "Number of selected passengers are:" . $_POST["passengers"];
+    // Your Slider value is here, do what you want with it. Mail/Print anything..
+} else {
+    Echo "Please slide the Slider Bar and Press Submit.";
+}
 
 function getData($topic)
 {
     $handle = fopen($topic . ".txt", "r");
     if ($handle) {
         $i = 0;
-        while($i < 20) {
-            /*while ((*/$line = fgets($handle);/*)) !== false) {*/
-                // process the line read.
-                $array = (explode("|", $line));
-                $value = $array[1];
-                echo $value;
+
+            $line = fgets($handle);
+            // process the line read.
+
+        $range = $_POST['passengers'];
+
+//            $trimmedLine = (implode("", array_slice(file($topic.".txt"), -1)));
+            $array = (explode("|", $line));
+
+            $file = escapeshellarg($topic . ".txt");
+            $last_line = `tail -n $range $file`;
+
+
+            echo $last_line;
+            $value = $array[1];
+            echo $value;
             $i++;
-            //}
-        }
         fclose($handle);
-    } else {
-        //Error handling
-    }
+    } else
+        echo 'No data found';
 }
+
+?>
--- subscribe.php
+++ subscribe.php
@@ -1,8 +1,6 @@
 <?php
-// subscribe.php
-require("models/phpMQTT.php");
+require("../team12-g7/models/phpMQTT.php");
 require("../team12-g7/database.php");
-
 
 $host = "146.87.2.99";
 $port = "1883";
@@ -10,72 +8,35 @@ date_default_timezone_set('Europe/London');
 $date_timezone = date_default_timezone_get();
 
 
+$mqtt = new phpMQTT($host, $port, "ClientID" . rand());
 
-
-
-$mqtt = new phpMQTT($host, $port, "ClientID".rand());
-
-if(!$mqtt->connect()){
+if (!$mqtt->connect()) {
     exit(1);
 }
 
-//while(true) {
-    $weather_factors = array("temp", "humidity", "air_pressure", "rain", "wind_s", "wind_d", "solar");
-    foreach ($weather_factors as $factor) {
-        $topics['/' . $factor] = array("qos" => 0, "function" => "procmsg");
-    }
-  //  sleep(1);
+$weather_factors = array("temp", "humidity", "air_pressure", "rain", "wind_s", "wind_d", "solar");
+foreach ($weather_factors as $factor) {
+    $topics['/' . $factor] = array("qos" => 0, "function" => "procmsg");
+}
 
-//}
-
-
-
-
-
-
-/*$topics['/temp'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/humidity'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/air_pressure'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/rain'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/wind_s'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/wind_d'] = array("qos"=>0, "function"=>"procmsg");
-$topics['/solar'] = array("qos"=>0, "function"=>"procmsg");*/
-
-$mqtt->subscribe($topics,0);
-while($mqtt->proc()){
-
+$mqtt->subscribe($topics, 0);
+while ($mqtt->proc()) {
 }
 $mqtt->close();
-/**
- * @param $topic this is the type of weather data e.g "solar"
- * @param $msg this is the type of weather data value e.g "5.6"
- */
-function procmsg($topic, $msg)
-{
 
-    echo "Msg Recieved: " . date("h:i:sa") . "\nTopic:{$topic}\n$msg\n";
+/**
+ * Output each factor with its value
+ * Write the results into its text file
+ * according to its respected factor
+ */
+function procmsg($factor, $value)
+{
+    echo "Time: " . date("h:i:sa") . "\n" . "Factor: {$factor}\n$value\n";
     $date = date("h:i:sa");
 
-    $myfile = fopen(__DIR__ . "/".$topic.".txt", "a") or die("Unable to open file!");
-    //
-    //$weather_factors = array("temp", "humidity", "air_pressure", "rain", "wind_s", "wind_d", "solar");
-    //$check_new_line = "New Line";
-    $weather_data =$topic . " | " . $msg . " | " . $date /*. PHP_EOL*/;
-        //$id_for_data++;
-
-
-
-
-    //$myfile = fopen(__DIR__ . "/weatherdata.txt", "w") or die("Unable to open file!");
-
-    //Showing where to locate files for data to be imported
-
-
-    fwrite($myfile, $weather_data."\n");
-
-
+    $myfile = fopen(__DIR__ . $factor . ".txt", "a") or die("Unable to open file!");
+    $weather_data = $factor . " | " . $value . " | " . $date /*. PHP_EOL*/;
+    fwrite($myfile, $weather_data . "\n");
     fclose($myfile);
-
-
 }
 
