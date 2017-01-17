73f5752756c038513b9bcc712c807a8683a2fe3d
ArifK1994 <m702@hormail.co.uk>
2017-01-17 17:28:20 +0000

    Showing last lines for each sensor

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
 <?php
-require("index.phtml");
+
+//function getData($topic)
+//{
+//    $handle = fopen($topic . ".txt", "r");
+//    if ($handle) {
+//        $line = fgets($handle);
+//            // process the line read.
+//            $array = (explode("|", $line));
+//            $value = $array[1];
+//            echo $value;
+//        //}
+//        fclose($handle);
+//    } else {
+//        //Error handling
+//    }
+//}
+
+
+
+
+
 
 function getData($topic)
 {
     $handle = fopen($topic . ".txt", "r");
     if ($handle) {
-        /*while ((*/$line = fgets($handle);/*)) !== false) {*/
-            // process the line read.
-            $array = (explode("|", $line));
-            $value = $array[1];
-            echo $value;
-        //}
+        $line = fgets($handle);
+        $array = (explode("", array_slice(file($topic . ".txt"), 1)));
+
+        $file = escapeshellarg($topic . ".txt");
+        $last_line = `tail -n 1 $file`;
+
+        echo $last_line;
+        $value = $array[1];
+        echo $value;
         fclose($handle);
-    } else {
-        //Error handling
-    }
+    } else
+        echo 'No data found';
+
 }
 
 
+
+
+
+
+
+require("index.phtml");
 
 ?>
