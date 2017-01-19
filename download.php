383c9ce70a9dc10a6552d3134724f71380f73e28
ArifK1994 <m702@hormail.co.uk>
2017-01-19 17:53:21 +0000

    refactored

--- download.php
+++ download.php
@@ -1,25 +1,56 @@
-<?php
-function downloadData($topic) {
-    $file = $topic.'.txt';
-    $newfile = $topic.'.csv';
+<?php require("downloads.phtml");
 
-//    if(isset($_GET['.download_btn'])){
-//        $link=$_GET['.download_btn'];
-//        if ($link == '1') {
-//
-//        }
+if (!isset($_POST["rangeSlider"]))
+    echo "Please slide the Slider Bar and Press Submit.";
 
-    if (!copy($file, $newfile)) {
-        echo "failed to copy";
-    }
+
+function downloadData($topic)
+{
+    $handle = fopen($topic . ".txt", "r");
+    if ($handle) {
+
+        $last_line = '';
+        $range = $_POST['rangeSlider'];
+
+        $array = (explode("", array_slice(file($topic . ".txt"), 1)));
+
+        $file = escapeshellarg($topic . ".txt");
+
+        // Navigate through the slider to select number of lines
+        if (isset($_POST['topLines']))
+            $last_line = `head -n $range $file`;
+        if (isset($_POST['lastLines']))
+            $last_line = `tail -n $range $file`;
+
+        // When clicked on download buttton, display the values of
+        // selected number of lines
+        if (isset($_POST['topLines']) || isset($_POST['lastLines'])) {
+
+            // Create a download window as CSV format
+            header("Content-Type: text/csv");
+            header("Content-Disposition: attachment; filename=file.csv");
+
+            // Input the data into the download file
+            $download_increment = 0;
+            $i = 0;
+            foreach ($array as $value) {
+                if ($i == 0) {
+
+                } elseif ($i % 2 == 0) {
+                    $array1 = (explode("/", $value));
+                    $weather_data = print $array1[0] . ",";
+                    $myfile = fopen(__DIR__ . "download" . $download_increment . ".csv", "a") or die("Unable to open file!");
+                    fputcsv($myfile, $weather_data . "\n");
+                    fclose($myfile);
+                } else {
+                    echo $value;
+                }
+                $i++;
+            }
+        }
+        echo $last_line;
+        fclose($handle);
+    } else
+        echo 'No data found';
 }
-
-function downloadAllData() {
-    $weather_factors = array("temp", "humidity", "air_pressure", "rain", "wind_s", "wind_d", "solar");
-
-    foreach($weather_factors as $factor) {
-        downloadData($factor);
-    }
-}
-
 ?>
