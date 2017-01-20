e10aae000a2b6148a9c43b1e5844d0b56f33d914
cccampbell <cam1997campbell@googlemail.com>
2017-01-20 15:21:57 +0000

    added set point class

---
+++ Setpoints.php
@@ -0,0 +1,47 @@
+<?php
+require("setPoints.phtml");
+/**
+ * Created by PhpStorm.
+ * User: CameronCampbell
+ * Date: 20/01/2017
+ * Time: 15:00
+ */
+class Setpoints
+{
+
+    var $value, $name;
+
+    function __construct($name)
+    {
+        $value = 0;
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+}
+
+require("setPoints.phtml");
+?>
